257f3f60a91c9c7bb0fc4bf0022e9e52135012f6
Владислав <vladstorm98@gmail.com>
2025-01-24 12:05:53 +0200

    Update commit

---
+++ src/practice/Homework2.java
@@ -0,0 +1,72 @@
+package practice;
+
+public class Homework2 {
+    public static void main(String[] args) {
+//        String hi = "                Hello   ";
+//        String world = " WoRld!";
+//        char newLine = '\n';
+//
+//        String result = hi.trim() + world.toLowerCase() + newLine;
+//        System.out.println(result.repeat(3));
+//
+//        ----------------------------------
+//
+//         for (int i = 1; i <101; i++) System.out.print(i + "a ");
+//
+//        ----------------------------------
+//
+//        Cat cat = new Cat(10, "Vasil");
+//        Dog dog = new Dog(14, "Cris");
+//
+//        System.out.println(cat.getAge());
+//        dog.setName("Mutter");
+//        System.out.println(dog.getName());
+//
+//        ----------------------------------
+//
+//        double increment = 0.01123;
+//        double result;
+//        int count = 0;
+//
+//        if (increment > 0) {
+//            for (result = 0; result < 1_000_000; result += increment) count++;
+//            System.out.println(count);
+//        }
+//        ----------------------------------
+//
+//
+//        int[] array = {1,1,1,1,1};
+//        for (int i = 0; i < array.length; i++) {
+//            if (i % 2 == 0) {
+//                array[i] = 0;
+//            }
+//        }
+//        System.out.println(Arrays.toString(array));
+//
+//
+//        ----------------------------------
+//
+//
+//        int[] array1 = {1,2,5,7,10};
+//        int[] array2 = {2,3,2,17,15};
+//
+//        int[] array3 = new int[5 * 3];;
+//        for (int i = 0; i < 5; i++) {
+//            array3[i] = array1[i];
+//            array3[i + 5] = array2[i];
+//            array3[i + 10] = array1[i] * array2[i];
+//        }
+//        System.out.println(Arrays.toString(array3));
+//
+//        ----------------------------------
+//
+//        String hello = "Hello world";
+//        System.out.println(hello.replace("l", "r").toUpperCase().substring(0, 8));
+//
+//        ----------------------------------
+
+
+
+    }
+
+}
