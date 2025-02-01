44d2e902441db83a7c7826ef3c91c943db4c005c
Владислав <vladstorm98@gmail.com>
2025-02-01 14:44:04 +0200

    Добавлены файлы в tasks после исправления .gitignore

---
+++ src/tasks/AmountUniqSymbols.java
@@ -0,0 +1,15 @@
+package tasks;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class AmountUniqSymbols {
+    public static void main(String[] args) {
+        // Подсчитать, сколько раз каждый символ встречается в строке.
+        String text = "I am walking through the bridge";
+        Map<String, Long> uniqCount = text.chars()
+                .mapToObj(String::valueOf)
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+        System.out.println(uniqCount);
+    }
+}
---
+++ src/tasks/IsPalindrome.java
@@ -0,0 +1,31 @@
+package tasks;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class IsPalindrome {
+    public static void main(String[] args) {
+        // Проверить, является ли строка палиндромом (читается одинаково слева направо и справа налево).
+        String text = "maradaram";
+        System.out.println(isPalindrome(text));
+    }
+
+    public static boolean isPalindrome(String text) {
+        List<Character> charList= new ArrayList<>();
+        for (char ch : text.toCharArray()) {
+            charList.add(ch);
+        }
+
+        Iterator<Character> iterator = charList.iterator();
+        ListIterator<Character> listIterator = charList.listIterator(charList.size());
+        while (iterator.hasNext() && listIterator.hasPrevious()) {
+            if (iterator.next() != listIterator.previous()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
