4d36fa5ea6ac840c82edd564ee91e940a0bd61c3
Владислав <vladstorm98@gmail.com>
2025-02-01 20:30:25 +0200

    Update commit

---
+++ src/tasks/from_01_02_2025/Figures_1.java
@@ -0,0 +1,46 @@
+package tasks.from_01_02_2025;
+
+public class Figures_1 {
+    public static void main(String[] args) {
+        // Создайте абстрактный класс Shape, который имеет абстрактный метод area(), который должен возвращать
+        // площадь фигуры. Создайте два класса: Circle и Rectangle, которые наследуют Shape и реализуют метод area()
+        // для вычисления площади круга и прямоугольника соответственно.
+        Circle circle = new Circle(13.4);
+        Rectangle rectangle = new Rectangle(20, 15.7);
+
+        System.out.println(circle.area());
+        System.out.println(rectangle.area());
+    }
+}
+
+abstract class Shape {
+    abstract double area();
+}
+
+class Circle extends Shape{
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return 3.14 * this.radius * this.radius;
+    }
+}
+
+class Rectangle extends Shape{
+    private double longSide;
+    private double shortSide;
+
+    public Rectangle(double longSide, double shortSide) {
+        this.longSide = longSide;
+        this.shortSide = shortSide;
+    }
+
+    @Override
+    double area() {
+        return this.longSide * this.shortSide;
+    }
+}
---
+++ src/tasks/from_01_02_2025/Inheritance_0.java
@@ -0,0 +1,38 @@
+package tasks.from_01_02_2025;
+
+public class Inheritance_0 {
+    public static void main(String[] args) {
+        // Создайте класс Animal, который имеет поле name и метод makeSound(), который выводит "Animal makes a sound".
+        // Затем создайте класс Dog, который наследуется от Animal и переопределяет метод makeSound() так, чтобы
+        // он выводил "Dog barks".
+        Dog barsik = new Dog("Barsik");
+        barsik.makeSound();
+    }
+}
+
+class Animal {
+    private final String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(super.getName() + " barks");
+    }
+}
