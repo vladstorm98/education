54dfac3956359000bb545c9daa1d16f0f0100f36
Владислав <vladstorm98@gmail.com>
2025-02-05 12:53:31 +0200

    Change holder's name

---
+++ src/tasks/multithreading/TaskScheduler/TaskPriority.java
@@ -0,0 +1,17 @@
+package tasks.multithreading.TaskScheduler;
+
+public enum TaskPriority {
+    HIGH (1),
+    MEDIUM (2),
+    LOW (3);
+
+    private int priority;
+
+    TaskPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
---
+++ src/tasks/multithreading/TrafficLightSimulator/TrafficLightSimulator.java
@@ -0,0 +1,40 @@
+package tasks.multithreading.TrafficLightSimulator;
+
+public class TrafficLightSimulator extends Thread{
+    private boolean running = true;
+    private TrafficLight currentLight = TrafficLight.GREEN;
+
+    @Override
+    public void run() {
+        while (running) {
+            System.out.println("Traffic light changed to " + currentLight);
+            try {
+                Thread.sleep(currentLight.getDuration());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            switchLight();
+        }
+    }
+
+    public void switchLight() {
+        currentLight = switch (currentLight) {
+            case GREEN -> currentLight = TrafficLight.RED;
+            case RED -> currentLight = TrafficLight.YELLOW;
+            case YELLOW -> currentLight = TrafficLight.GREEN;
+        };
+    }
+
+    public void stopTrafficLight() {
+        System.out.println("Traffic light switched off");
+        running = false;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public TrafficLight getCurrentLight() {
+        return currentLight;
+    }
+}
